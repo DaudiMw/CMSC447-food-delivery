d103648aefb3ac768230d5158b4714090056fac2
William-Zendgraft <williaz1@umbc.edu>
2025-10-22 15:44:29 -0400

    Use a base page now

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,13 +1,22 @@
-class MyApp extends React.Component {
+class BasePage extends React.Component {
     render() {
         return (
-            <div className="content">
-                <div className="header">
-                    <button className="button">
+            <div className="basepage">
+                <div className="banner">
+                    <img className="logo" src="UMBCLogo.png"></img>
+                    <button className="restaurantButton">
                         Restaurants
                     </button>
                 </div>
             </div>
+        )
+    }
+}
+
+class MyApp extends React.Component {
+    render() {
+        return (
+            <BasePage />
         )
     }
 };
