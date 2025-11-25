447b3a2a229a3d346e2f1a084488085826b839ca
William-Zendgraft <williaz1@umbc.edu>
2025-11-25 16:07:14 -0500

    Get from database, error present

--- Frontend/OrdersPage.js
+++ Frontend/OrdersPage.js
@@ -1,42 +1,46 @@
 class OrdersPage extends React.Component {
-    componentDidMount() {
-        let data = this.props.GetOrders();
+    async componentDidMount() {
+        const user_id = this.props.match.params.user_id
+        console.log(user_id);
+        
+        let data = await get_user_orders_by_id(user_id);
+        console.log(data)
         const orderList = document.getElementsByClassName("ordersList")[0];
-        data.forEach(element => {
-            const orderDiv = document.createElement('div');
-            orderDiv.className = "order";
+        // data.forEach(element => {
+        //     const orderDiv = document.createElement('div');
+        //     orderDiv.className = "order";
 
-            const right = document.createElement('div');
-            right.className = "orderRight";
-            orderDiv.appendChild(right);
-            const checkbox = document.createElement('input');
-            checkbox.className = "ordersCheckbox";
-            checkbox.type = "checkbox"
-            right.appendChild(checkbox);
+        //     const right = document.createElement('div');
+        //     right.className = "orderRight";
+        //     orderDiv.appendChild(right);
+        //     const checkbox = document.createElement('input');
+        //     checkbox.className = "ordersCheckbox";
+        //     checkbox.type = "checkbox"
+        //     right.appendChild(checkbox);
 
-            const middle = document.createElement('div');
-            middle.className = "orderMiddle";
-            middle.textContent = "Description";
-            orderDiv.appendChild(middle);
-            const info = document.createElement('div');
-            info.className = "orderInfo";
-            const status = document.createElement('div');
-            status.textContent = "Status: Pending";
-            info.appendChild(status);
-            const total = document.createElement('div');
-            total.textContent = "Total: $XX.XX";
-            info.appendChild(total);
-            const arrivalTime = document.createElement('div');
-            arrivalTime.textContent = "Estimated Arrival: XX:XXam/pm";
-            info.appendChild(arrivalTime);
-            middle.appendChild(info);
+        //     const middle = document.createElement('div');
+        //     middle.className = "orderMiddle";
+        //     middle.textContent = "Description";
+        //     orderDiv.appendChild(middle);
+        //     const info = document.createElement('div');
+        //     info.className = "orderInfo";
+        //     const status = document.createElement('div');
+        //     status.textContent = "Status: Pending";
+        //     info.appendChild(status);
+        //     const total = document.createElement('div');
+        //     total.textContent = "Total: $XX.XX";
+        //     info.appendChild(total);
+        //     const arrivalTime = document.createElement('div');
+        //     arrivalTime.textContent = "Estimated Arrival: XX:XXam/pm";
+        //     info.appendChild(arrivalTime);
+        //     middle.appendChild(info);
 
-            const image = document.createElement('img');
-            image.className = "orderImage";
-            orderDiv.appendChild(image);
+        //     const image = document.createElement('img');
+        //     image.className = "orderImage";
+        //     orderDiv.appendChild(image);
 
-            orderList.appendChild(orderDiv);
-        })
+        //     orderList.appendChild(orderDiv);
+        // })
     }
 
     render() {
@@ -52,5 +56,19 @@ class OrdersPage extends React.Component {
                 </div>
             </div>
         )
+    }
+}
+
+/**
+ * Function to get user's orders by user ID
+ * @param {*} userId 
+ * @returns 
+ */
+async function get_user_orders_by_id(userId) {
+    try {
+        const response = await authFetch(`/${userId}/orders`, { method: 'GET' });
+        return response;
+    } catch (error) {
+        console.log(error)
     }
 }
