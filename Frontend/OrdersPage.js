1b282c7ee04fe58629b2bc01dd9dc20c90a05291
William-Zendgraft <williaz1@umbc.edu>
2025-11-29 18:04:52 -0500

    Order page order amount checking

--- Frontend/OrdersPage.js
+++ Frontend/OrdersPage.js
@@ -6,41 +6,44 @@ class OrdersPage extends React.Component {
         let data = await get_user_orders_by_id(user_id);
         console.log(data)
         const orderList = document.getElementsByClassName("ordersList")[0];
-        // data.forEach(element => {
-        //     const orderDiv = document.createElement('div');
-        //     orderDiv.className = "order";
+        if (data.length == 0) {
+            return;
+        }
+        data.forEach(element => {
+            const orderDiv = document.createElement('div');
+            orderDiv.className = "order";
 
-        //     const right = document.createElement('div');
-        //     right.className = "orderRight";
-        //     orderDiv.appendChild(right);
-        //     const checkbox = document.createElement('input');
-        //     checkbox.className = "ordersCheckbox";
-        //     checkbox.type = "checkbox"
-        //     right.appendChild(checkbox);
+            const right = document.createElement('div');
+            right.className = "orderRight";
+            orderDiv.appendChild(right);
+            const checkbox = document.createElement('input');
+            checkbox.className = "ordersCheckbox";
+            checkbox.type = "checkbox"
+            right.appendChild(checkbox);
 
-        //     const middle = document.createElement('div');
-        //     middle.className = "orderMiddle";
-        //     middle.textContent = "Description";
-        //     orderDiv.appendChild(middle);
-        //     const info = document.createElement('div');
-        //     info.className = "orderInfo";
-        //     const status = document.createElement('div');
-        //     status.textContent = "Status: Pending";
-        //     info.appendChild(status);
-        //     const total = document.createElement('div');
-        //     total.textContent = "Total: $XX.XX";
-        //     info.appendChild(total);
-        //     const arrivalTime = document.createElement('div');
-        //     arrivalTime.textContent = "Estimated Arrival: XX:XXam/pm";
-        //     info.appendChild(arrivalTime);
-        //     middle.appendChild(info);
+            const middle = document.createElement('div');
+            middle.className = "orderMiddle";
+            middle.textContent = "Description";
+            orderDiv.appendChild(middle);
+            const info = document.createElement('div');
+            info.className = "orderInfo";
+            const status = document.createElement('div');
+            status.textContent = "Status: Pending";
+            info.appendChild(status);
+            const total = document.createElement('div');
+            total.textContent = "Total: $XX.XX";
+            info.appendChild(total);
+            const arrivalTime = document.createElement('div');
+            arrivalTime.textContent = "Estimated Arrival: XX:XXam/pm";
+            info.appendChild(arrivalTime);
+            middle.appendChild(info);
 
-        //     const image = document.createElement('img');
-        //     image.className = "orderImage";
-        //     orderDiv.appendChild(image);
+            const image = document.createElement('img');
+            image.className = "orderImage";
+            orderDiv.appendChild(image);
 
-        //     orderList.appendChild(orderDiv);
-        // })
+            orderList.appendChild(orderDiv);
+        })
     }
 
     render() {
